026c6c92fc91af2d9502172bc625fc472bcf2f95
equinaut <matthewamos@live.com>
2018-08-13 06:54:14 -0400

    create deck with nested lists

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,28 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-  @doc """
-  Hello world.
+    # List comprehensions
+    # Each element => do block => loaded into new list
+    # This returns a nested list - needs to be passed to List.flatten
+    cards = for suit <- suits do
+      for value <- values do
+        # Joining strings
+        # value <> " of " <> suit
+        # String interpolation
+        "#{value} of #{suit}"
+      end
+    end
 
-  ## Examples
+    List.flatten(cards)
+  end
 
-      iex> Cards.hello
-      :world
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
 
-  """
-  def hello do
-    :world
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
